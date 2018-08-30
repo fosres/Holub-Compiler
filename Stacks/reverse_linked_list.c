4308fddfd57b925377298e0ff9363abac743ac33
tanveerasalim <32973884+tanveerasalim@users.noreply.github.com>
2018-08-29 19:47:03 -0500

    Update reverse_linked_list.c

--- Stacks/reverse_linked_list.c
+++ Stacks/reverse_linked_list.c
@@ -2,9 +2,62 @@
 #include <stdlib.h>
 #include "sll.h"
 
+
+int sll_len(Node * first)
+{
+  int len = 0;
+  
+  while ( first != NULL )
+  {
+    len++;
+
+    first = first->link;
+  }
+
+  return len;
+}
+
+int remove_node(Node ** rootp, Node * node)
+{
+
+  Node * first = *rootp;
+
+  while ( (*rootp) != NULL )
+  {
+    if ( (*rootp) == node )
+    {
+      if ( (*rootp) == first )
+      { 
+        (*rootp)->x = (*(rootp+1))->x; 
+
+        (*rootp)->link = (*(rootp+1))->link;
+        
+      }
+
+      else if ( (*rootp)->link == NULL )
+      { (*(rootp - 1))->link = NULL; }
+      
+      else
+      {
+        (*(rootp - 1))->link =  (*(rootp + 1));
+      }
+
+      return 1;
+    }
+
+    rootp++;
+  }
+
+  return 0;
+
+}
+
+#if 0
 Node * reverse_sll(Node * first)
 {
-  Node * arr[1000]; Node ** arr_p = &arr[0] - 1;
+ {
+    
+ } Node ** arr_p = &arr[0] - 1;
 
   while (first != NULL )
   {
@@ -25,8 +78,10 @@ Node * reverse_sll(Node * first)
 
   return new_head;
 }
+#endif
 
 int main(void) {
+  
   Node * a = (Node *)malloc(sizeof(Node));
 
   a->x = 3;
@@ -53,7 +108,40 @@ int main(void) {
 
   Node * first = a;
 
+  #if 0
+  
   Node * rev = reverse_sll(a);
 
+  
   printf("%d\n",rev->link->link->link->link->x);
+  
+  #endif
+
+  Node * node_arr[1000];
+
+  node_arr[0] = a;
+
+  node_arr[1] = b;
+
+  node_arr[2] = c;
+
+  node_arr[3] = d;
+
+  node_arr[4] = NULL;
+
+  Node ** node_arr_p = node_arr;
+
+#if 0
+  printf("%d\n",(*node_arr_p++)->x);
+  
+  printf("%d\n",(*node_arr_p++)->x);
+
+  printf("%d\n",(*node_arr_p)->x);
+
+#endif
+
+
+remove_node(node_arr,a);
+printf("%d\n",a->link->x);
+  
 }

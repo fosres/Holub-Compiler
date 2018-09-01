90def71e9d15e03b877dba8ba953ba7491c0e43b
tanveerasalim <32973884+tanveerasalim@users.noreply.github.com>
2018-09-01 16:26:50 -0500

    Create delete_node_sll.c

---
+++ Stacks/delete_node_sll.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define item int
+
+typedef struct Node
+{ struct Node * link;
+
+  item x;
+} Node;
+
+int sll_remove(Node * rootp, Node * node)
+{
+  Node * first = rootp;
+
+  Node * previous = (Node *)malloc(sizeof(Node));
+
+  while ( rootp != NULL )
+  {
+    if ( rootp == node )
+    {
+      
+      if ( rootp == first && first->link == NULL )
+      {
+      Node ** first_make_NULL = &first;
+
+      (*first_make_NULL) = NULL;
+      }
+    
+
+    else if ( rootp->link == NULL )
+    {
+      previous->link = NULL;
+    }
+
+    else //node is between first and last nodes
+    {
+      rootp->x = rootp->link->x;
+
+      rootp->link = rootp->link->link;
+    }
+
+    return 1;
+
+    }
+
+    previous = rootp; rootp= rootp->link;
+
+  }
+
+  return 0;
+
+}
+
+int main(void) {
+  Node * head = malloc(sizeof(Node));
+
+  head->x = 3;
+
+  Node * b = malloc(sizeof(Node));
+
+  b->x = 5;
+
+  Node * c = malloc(sizeof(Node));
+
+  c->x = 6;
+
+  head->link = b;
+
+  b->link = c;
+
+  c->link = NULL;
+
+  #if 0
+
+  sll_remove(head,head);
+
+  printf("%d\n",head->x);
+
+  printf("%d\n",head->link->x);
+
+  sll_remove(head,b);
+
+  printf("%d\n",head->link->x);
+
+  #endif
+
+  sll_remove(head,c);
+
+  printf("%d\n",head->link->x);
+
+  printf("%d\n",head->link->link->x);
+
+  return 0;
+}

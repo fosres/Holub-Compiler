0032654e77b9ce05696bd44d3da148cc588e3d78
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-08-10 15:52:43 -0500

    master

--- ch2/lexyy.c
+++ ch2/lexyy.c
@@ -168,4 +168,52 @@ static YY_TYPE Yy_nxt[6][128] =
 
 #define yy_next(state,c) Yy_next[state][c]
 
-	  
+#if 0
+ * The Yy_cmap[] and Yy_rmap arrays are
+ * used as follows:
+ *
+ * next_state= Yy_nxt[Yy_rmap[current_state] ][ Yy_cmap[input_char] ];
+ *
+ * Character positions in the Yy_cmap array are:
+ */
+
+Character positions in the Yy_cmap array are:
+
+^@ ^A ^B ^C ^D ^E ^F ^G ^H ^I ^J ^K ^L ^M ^N ^O
+^P ^Q ^R ^S ^T ^U ^V ^W ^X ^Y ^Z ^[ ^\ ^] ^^ ^_
+    !  "  #  $  %  &  '  (  )  *  +  ,  -  .  /
+ 0  1  2  3  4  5  6  7  8  9  :  ;  <  =  >  ?
+ @  A  B  C  D  E  F  G  H  I  J  K  L  M  N  O
+ P  Q  R  S  T  U  V  W  X  Y  Z  [  \  ]  ^  _
+ '  a  b  c  d  e  f  g  h  i  j  k  l  m  n  o
+ p  q  r  s  t  u  v  w  x  y  z  {  |  }  ~  DEL
+
+#endif
+
+static unsigned char Yy_cmap[128] =
+{0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,
+ 0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,
+ 0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,0, 0, 1, 0,
+ 2, 2, 2, 2,2, 2, 2, 2,2, 2, 0, 0,0, 0, 0, 0,
+ 0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,
+ 0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,
+ 0, 0, 0, 0,0, 3, 0, 0,0, 0, 0, 0,0, 0, 0, 0,
+ 0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,0, 0, 0, 0,
+};
+
+static unsigned char Yy_nxt[5][4]
+=
+{
+/*00*/ {-1, 3, 1, -1 },
+/*01*/ {-1, 2, 1, 5 },
+/*02*/ {-1, -1, 2, 5},
+/*03*/ {-1, -1, 2,-1},
+/*04*/ {-1, -1, 4, -1}
+};
+
+#if 0
+yy_next(state,c) is given the current state number and
+evaluates to the next state.
+#endif
+
+#define yy_next(state,c) (Yy_nxt[Yy_rmap[state]][Yy_cmap[c]])

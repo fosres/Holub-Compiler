96ae7c40d03f023300537fc2a6f8a018b77b8782
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-08-20 20:02:56 -0500

    master

---
+++ ch2/globals.h
@@ -0,0 +1,24 @@
+/*GLOBALS.H: Gloal variables shared between modules */
+#ifdef ALLOC
+#	define CLASS
+#	define I(x) (x)
+#else 	
+#	define CLASS extern
+#	define I(x)
+#endif
+#define MAXINP	2048
+
+CLASS int Verbose I( = 0);
+CLASS int No_lines I( = 0);
+CLASS int Unix	I( = 0);
+CLASS int Public I( = 0);
+CLASS char *Template I(="lex.par");
+CLASS int Actual_lineno I( = 1);
+CLASS int Lineno I( = 1);
+
+CLASS char Input_buf[MAXINP];
+CLASS char*Input_file_name;
+CLASS FILE*Ifile;
+CLASS FILE*Ofile;
+#undef CLASS
+#undef I
---
+++ ch2/nfa.h
@@ -0,0 +1,45 @@
+/*----------------------------------
+ * Nfa state;
+ */
+
+//typedef unsigned short     _SETTYPE ;		   /* one cell in bit map   */
+
+typedef struct _set_
+{
+    unsigned short defmap[8];	/* The map itself		  */
+    unsigned short *map   ;		/* Pointer to the map		  */
+    unsigned	  nbits  ;		/* Number of bits in map	  */
+    unsigned char nwords ;		/* Number of words in map	  */
+    unsigned char compl	 ;		/* is a negative true set if true */
+
+} SET;
+
+typedef struct nfa
+{
+	SET *bitset; /*Set to store character classes. */
+	struct nfa *next; /*Next state (or NULL if none) */
+	struct nfa *next2; /*Another next state if edge==EPSILON */
+	char * accept;/*NULL if not an accepting state, else */
+	int anchor;/*Says whether pattern is anchored and, if */
+		   /*so, where (uses #defines above)	*/
+	int edge;	
+
+} NFA;
+
+#define EPSILON -1
+#define CCL -2
+#define EMPTY -3
+
+#define NONE 0
+#define START 1
+#define END 2
+#define BOTH (START|END)
+
+#define NFA_MAX 768 /*Maximum number of NFA states in a */
+#define STR_MAX (10*1024)
+
+void new_macro(char*definition);
+void printmacs(void);
+NFA *thompson(char*(*input_funct)(),int*max_state,NFA**start_state);
+void print_nfa(NFA*nfa,int len,NFA*start);
+

dd9d6daa322a7b70ba3fedd9826e0862132c2568
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-08-24 01:13:37 -0500

    master

--- ch1/exercise_1-5/uint.h
+++ ch1/exercise_1-5/uint.h
@@ -11,6 +11,17 @@ typedef unsigned short uint16_t;
 #define __uint16_t_defined 1
 #endif
 
+#ifndef __uint32_t_defined
+typedef unsigned uint32_t;
+#define __uint32_t_defined 1
+#endif
+
+#if 0
+#ifndef __uint64_t_defined
+typedef unsigned long long int uint64_t;
+#define __uint64_t_defined 1
+#endif
+#endif
 
 	
 #endif //__UINT_H__
